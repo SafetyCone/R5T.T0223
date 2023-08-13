c975f0383192a641c8ee4eb2d0811ce20e18d2db
davidcoats <david@getsafetycone.com>
2023-08-12 23:24:23 -0700

    Interim changes.

---
+++ source/R5T.T0223/Code/Attributes/RazorLinkMarkerAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using R5T.T0143;
+
+
+namespace R5T.T0223
+{
+    /// <summary>
+    /// Attribute indicating a type is a Razor link.
+    /// It's usually pretty clear that something is a Razor link, but the marker attribute is useful for surveying assemblies and code, then building a catalogue of Razor links.
+    /// </summary>
+    [AttributeUsage(
+        // Only class, since Razor components are only classes.
+        AttributeTargets.Class,
+        AllowMultiple = false, Inherited = false)]
+    [MarkerAttributeMarker]
+    public class RazorLinkMarkerAttribute : Attribute,
+        IMarkerAttributeMarker
+    {
+        /// <summary>
+        /// Allows specifying that a type is *not* a Razor link type.
+        /// This is useful for marking types that end up canonical Razor link code file locations, but are not actually Razor links.
+        /// </summary>
+        public bool IsRazorLink { get; }
+
+
+        public RazorLinkMarkerAttribute(
+            bool isRazorLink = true)
+        {
+            this.IsRazorLink = isRazorLink;
+        }
+    }
+}
